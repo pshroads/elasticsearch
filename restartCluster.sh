35b09cdb8c1c17c836364c5d63633210b2767254
Patrick Shroads <pshroads@users.noreply.github.com>
2017-04-26 14:44:07 -0400

    Add files via upload
    Adding support for authentication.

--- restartCluster.sh
+++ restartCluster.sh
@@ -4,7 +4,10 @@ HOST=$1 # Any node in the cluster
 
 ES_HEALTH_URL="http://$HOST:9200/_cat/health"
 
-read clusterName clusterHealth relocatingShards initializingShards unassignedShards pendingTasks <<< $(curl -s $ES_HEALTH_URL | awk '{print $3 " " $4 " " $9 " " $10 " " $11 " " $12}')
+ESUSER=$(whoami)
+read -p "Password for $ESUSER:" -s PWD
+
+read clusterName clusterHealth relocatingShards initializingShards unassignedShards pendingTasks <<< $(curl -u $ESUSER:$PWD -s $ES_HEALTH_URL | awk '{print $3 " " $4 " " $9 " " $10 " " $11 " " $12}')
 echo "$(date +%Y-%m-%d_%H:%M:%S) - Cluster state is $clusterHealth ($relocatingShards relocating shards, $initializingShards initializing shards, $unassignedShards unassigned shards, $pendingTasks pending tasks)."			
 echo "!!! ALL NODES WILL BE STOPPED AND RESTARTED !!!"
 read -p "Restart cluster $clusterName? Y or N:" -n 2 -r
@@ -13,7 +16,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 	then
 		ES_NODES_URL="http://$HOST:9200/_cat/nodes"
 	    echo "$(date +%Y-%m-%d_%H:%M:%S) - Starting cluster restart...."
-	    clusterNodes=$(curl -s -XGET $ES_NODES_URL | awk '{print $1}' | sort)
+	    clusterNodes=$(curl -u $ESUSER:$PWD -s -XGET $ES_NODES_URL | awk '{print $1}' | sort)
 	    
 	    for clusterNode in $clusterNodes
 	    do
